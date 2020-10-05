4dc98443856da536e2f0c9649b9a7b9d57177539
Ellen <emans@hotmail.nl>
2020-10-05 16:07:53 +0200

    aangepast path in preprocessor

--- eigen_project/preprocessing-cp/resources/preprocessor.py
+++ eigen_project/preprocessing-cp/resources/preprocessor.py
@@ -26,7 +26,7 @@ def clean(dataset):
     data_repo = os.environ['DATA_REPO']
     if data_repo:
         file_path = os.path.join(data_repo)
-        dataset.to_json(file_path)
+        dataset.to_json(file_path + "/preprocessed_data.json")
     else:
         dataset.to_json("preprocessed_data.json")
         return json.dumps({'message': 'The data was saved locally.'},
